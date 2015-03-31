3956c05eb85f1a1be8e128488e021da8f8fca8e7
gorazio <gmail@gorazio.com>
2015-03-31 17:17:25 +0300

    add versions to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "gmail@gorazio.com"
 license          "MIT"
 description      "Installs/configures nginx"
 long_description "Please refer to README.md"
-version          "3.3.6"
+version          "3.3.7"
 
 recipe "nginx", "The default recipe which sets up the repository."
 recipe "nginx::configuration", "Internal recipe to setup the configuration files."
@@ -12,6 +12,6 @@ recipe "nginx::service", "Internal recipe to setup the service definition."
 recipe "nginx::server", "Install and configure the `nginx` package."
 recipe "nginx::debug", "Install and configure the `nginx-debug` package."
 
-depends 'apt'
-depends 'logrotate'
-depends 'firewall'
+depends 'apt', '~> 2.7.0'
+depends 'logrotate', '~> 1.9.1'
+depends 'firewall', '= 0.11.8'
